84835fbf7bbbeb59ee205df390d3c0868c213191
Luis E Ogando <elgreatness23@gmail.com>
2022-06-02 23:58:47 -0400

    Adding Project on github to practice github again

---
+++ utils.js
@@ -0,0 +1,15 @@
+// ? This fuction will delay the Fetch request for API
+// ? So we can avoid  multiple request
+
+
+const debounce = (func, delay = 1000) => {
+  let timeoutId;
+  return (...args) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      func.apply(null, args);
+    }, delay);
+  };
+};
